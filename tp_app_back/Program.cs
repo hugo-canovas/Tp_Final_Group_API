34938c336ec45c18b0daa8007424f296f2acd4ea
chinokira <adrisqf@gmail.com>
2025-01-10 14:01:52 +0100

    add jwt token

--- tp_app_back/Program.cs
+++ tp_app_back/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using tp_app_back.Data;
 using tp_app_back.Interfaces;
 using tp_app_back.Services;
+using tp_app_back.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +16,43 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     //.UseLazyLoadingProxies()
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//remove warning to create roles
+// Remove warning to create roles
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //    options.ConfigureWarnings(warnings =>
 //        warnings.Ignore(RelationalEventId.PendingModelChangesWarning)));
 
+builder.Services.Configure<JwtSetting>(builder.Configuration.GetSection("Jwt"));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    var secretKey = builder.Configuration["Jwt:SecretKey"];
+    var issuer = builder.Configuration["Jwt:Issuer"];
+    var audience = builder.Configuration["Jwt:Audience"];
+
+    if (string.IsNullOrEmpty(secretKey) || string.IsNullOrEmpty(issuer) || string.IsNullOrEmpty(audience))
+    {
+        throw new ArgumentException("les paramètres jwt doivent être définis");
+    }
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey)),
+    };
+});
+
 // Add services to the container.
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IJwtService, JwtService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -31,7 +64,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
     {
-        policy.WithOrigins("http://localhost:4200") 
+        policy.WithOrigins("http://localhost:4200")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
@@ -51,6 +84,8 @@ app.UseHttpsRedirection();
 // Appliquez la politique CORS
 app.UseCors("AllowAngularApp");
 
+// Add Authentication Middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ tp_app_back/Services/JwtService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using tp_app_back.Interfaces;
+using tp_app_back.Models;
+
+public class JwtService : IJwtService
+{
+    private readonly JwtSetting _jwtSetting;
+
+    public JwtService(IOptions<JwtSetting> jwtSetting)
+    {
+        _jwtSetting = jwtSetting.Value;
+    }
+    public string GenerateToken(Employee employee)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, employee.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim("name", $"{employee.FirstName} {employee.LastName}")
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSetting.SecretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _jwtSetting.Issuer,
+            audience: _jwtSetting.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
